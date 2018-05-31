562d5906d04ff05903c7acb855b384b92f678d28
abimaelhernandez <hernandezabimael364@gmail.com>
2018-05-31 01:52:52 -0500

    set state for arr

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,16 @@ import React,{ Component } from 'react';
 export default class App extends Component {
   constructor(props){
     super(props)
+      this.state = {
+      arreglo : [1,4,57,96,4,5,5,98,]
     }
-
+  }
 
   render () {
     return (
       <div >
-      Hello World
+      Hello World sadasdasdasdad]
+      {this.state.arreglo[5]}
       </div>
     )
   }
